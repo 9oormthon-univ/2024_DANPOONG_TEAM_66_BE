f4683e53b9f53e8663b0de9925af16ab38764899
hy30nq <121498814+hy30nq@users.noreply.github.com>
2024-11-24 03:03:39 +0900

    Delete shema.sql

--- shema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS tasks (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    image VARCHAR(255) NOT NULL,
-    company VARCHAR(100) NOT NULL,
-    title VARCHAR(100) NOT NULL,
-    description TEXT NOT NULL,
-    badgeType ENUM('Normal', 'Gold', 'Silver', 'Bronze') NOT NULL
-);
